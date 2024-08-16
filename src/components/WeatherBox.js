9d197b8bb6bdc9b92ca04381eae26b56af07fe4e
arkim <arkim0630@gmail.com>
2024-08-16 13:06:52 +0900

    ui 및 빌드시 오류나는 부분

--- src/components/WeatherBox.js
+++ src/components/WeatherBox.js
@@ -1,15 +1,12 @@
-import React, {useEffect, useState} from 'react'
+import React from 'react'
 
 export default function Weather({ weather }) {
-
-    const [ tempC, setTempC ] = useState('');
-    const [ tempF, setTempF ] = useState('');
 
     const weatherIcon = () =>{
         if(weather !== undefined || weather !== '') {
             let url = `https://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`
             return (
-                <img alt="weather icon" src={url} />
+                <img alt="weather icon" src={url} style={{width:125}}/>
             )
         } else {
             return null;
@@ -17,47 +14,30 @@ export default function Weather({ weather }) {
     }
 
 
-
-    useEffect(() => {
-        const getTempSub = () => {
-            if(weather === undefined || weather === null) {
-                return null;
-            } else {
-                setTempC((weather.main.temp).toString());
-                setTempF(((weather.main.temp * 9/5) + 32).toString());
-
-                if(tempC.length > 5) {
-                    setTempC(tempC.substring(0, 5));
-                } else if (tempF.length > 5) {
-                    setTempF(tempF.substring(0, 5));
-                }
-            }
-        }
-        getTempSub();
-    }, [tempC, tempF, weather]);
-
-        return (
+    return (
         <div className='main_top'>
-            <div className="top-left">
-                {weatherIcon()}
-            </div>
             <div className='weather'>
-                <div className='temp'>
-                    <div className='temp item'>섭씨
-                        <div>{tempC ? tempC : 0} (℃)</div>
+                <div className='temp_info'>
+                    <div className="weather-icon">
+                        {weatherIcon()}
+                    </div>
+                    <div className="temp-container">
+                        <div className='temp'>
+                            <div className='temp item'>섭씨
+                                <div>{weather ? Math.round(weather.main.temp).toFixed(2) : 0} (℃)</div>
+                            </div>
+                        </div>
+                        <div className='temp'>
+                            <div className='temp item'>화씨
+                                <div>{weather ? Math.round((weather.main.temp * 9 / 5) + 32).toFixed(2) : 0} (℉)</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className='temp'>
-                    <div className='temp item'>화씨
-                        <div>{tempF ? tempF : 0} (℉)</div>
-                    </div>
+                <div className='city'>
+                    <h4>{weather && weather.name}&nbsp;&nbsp;/
+                        &nbsp;&nbsp;{weather?.weather[0].main}({weather?.weather[0].description})</h4>
                 </div>
-            </div>
-            <div className="top-right">
-                <div className='city'>{weather && weather.name} <br/>
-                    &nbsp;&nbsp;/&nbsp;&nbsp;{weather?.weather[0].main}({weather?.weather[0].description})
-                </div>
-
             </div>
         </div>
     )
